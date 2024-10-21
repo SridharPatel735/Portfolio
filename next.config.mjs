d58b9e9ff7eb343187c1a1994a2e476e74248c2c
Sridhar Patel <55153753+SridharPatel735@users.noreply.github.com>
2024-10-21 19:12:18 -0400

    updating image path logic

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+    basePath: isProd ? "/Portfolio" : "",
     output: 'export',
     distDir: 'dist', // GitHub Pages serves from the 'docs' directory by default
     images: {
