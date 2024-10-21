396b558145223fe70e4adc7ea02826ee98e2065c
Sridhar Patel <55153753+SridharPatel735@users.noreply.github.com>
2024-10-21 18:00:58 -0400

    modify nextjsworkflow

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'docs', // GitHub Pages serves from the 'docs' directory by default
+    distDir: 'dist', // GitHub Pages serves from the 'docs' directory by default
     images: {
         unoptimized: true, // Disable image optimization for static export
     },
