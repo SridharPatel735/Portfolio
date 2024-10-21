f089c281e59ebe6a5d8d653b7d8ae01de7e5f793
Sridhar Patel <55153753+SridharPatel735@users.noreply.github.com>
2024-10-21 18:14:27 -0400

    modify nextjsworkflow

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,6 @@ const nextConfig = {
 export default withSentryConfig(nextConfig, {
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
-
     // Suppresses source map uploading logs during build
     silent: true,
     org: "javascript-mastery",
